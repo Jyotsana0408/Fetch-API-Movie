ee5b3117a9e641c891483b816a5bb0ee3f44e5c6
Jyotsna0408 <jyotsnasahu16@gmail.com>
2023-08-25 13:00:18 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -8,6 +8,41 @@ function App() {
   const [error, setError] = useState(null);
   const [retryCount, setRetryCount] = useState(0);
   const [retryIntervalId, setRetryIntervalId] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setRetryCount(0);
+    } catch (error) {
+      setError(error.message);
+      setRetryCount((prevRetryCount) => prevRetryCount + 1);
+    }
+    setIsLoading(false);
+  }, []);
+
+  const cancelRetryHandler = useCallback(() => {
+    clearInterval(retryIntervalId);
+    setRetryCount(0);
+  }, [retryIntervalId]);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   useEffect(() => {
     if (retryCount > 0) {
@@ -21,66 +56,28 @@ function App() {
     return () => {
       clearInterval(retryIntervalId);
     };
-  }, [retryCount]);
+  }, [retryCount, fetchMoviesHandler]);
 
-  async function fetchMoviesHandler() {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://swapi.dev/api/films/');
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const data = await response.json();
-
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
-      setRetryCount(0);
-    } catch (error) {
-      setError(error.message);
-      setRetryCount(retryCount + 1);
+  const content = useMemo(() => {
+    if (movies.length > 0) {
+      return <MoviesList movies={movies} />;
+    } else if (error) {
+      return (
+        <React.Fragment>
+          <p>{error}</p>
+          <button onClick={fetchMoviesHandler}>Retry</button>
+          <button onClick={cancelRetryHandler}>Cancel</button>
+        </React.Fragment>
+      );
+    } else if (isLoading) {
+      return <p>Loading...</p>;
+    } else {
+      return <p>Found no movies.</p>;
     }
-    setIsLoading(false);
-  }
-
-  function cancelRetryHandler() {
-    clearInterval(retryIntervalId);
-    setRetryCount(0);
-  }
-
-  let content = <p>Found no movies.</p>;
-
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
-
-  if (error) {
-    content = (
-      <React.Fragment>
-        <p>{error}</p>
-        <button onClick={fetchMoviesHandler}>Retry</button>
-        <button onClick={cancelRetryHandler}>Cancel</button>
-      </React.Fragment>
-    );
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
+  }, [movies, error, isLoading, fetchMoviesHandler, cancelRetryHandler]);
 
   return (
     <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
       <section>{content}</section>
     </React.Fragment>
   );
