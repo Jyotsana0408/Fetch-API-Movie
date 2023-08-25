2d3217e4a012eb22aec13861619f42a897504c3d
Jyotsna0408 <jyotsnasahu16@gmail.com>
2023-08-25 13:20:52 +0530

    fifth commit

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Movie from './Movie';
 import classes from './MoviesList.module.css';
+
 const MovieList = (props) => {
+
+const deleteHandler=(id)=>
+{
+  props.deleteId(id)
+}
+
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
@@ -10,6 +17,7 @@ const MovieList = (props) => {
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
+          deleteMovie={deleteHandler.bind(this,movie.id)}
         />
       ))}
     </ul>
