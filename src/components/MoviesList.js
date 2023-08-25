ee1e16c99271d20a753de69ee7b5edab1f2aefac
Jyotsna0408 <jyotsnasahu16@gmail.com>
2023-08-25 12:53:51 +0530

    second commit

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,14 +1,12 @@
-import React from "react";
-
-import Movie from "./Movie";
-
-import classes from "./MoviesList.module.css";
-
+import React from 'react';
+import Movie from './Movie';
+import classes from './MoviesList.module.css';
 const MovieList = (props) => {
   return (
-    <ul className={classes["movies-list"]}>
+    <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
+          key={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
@@ -17,5 +15,4 @@ const MovieList = (props) => {
     </ul>
   );
 };
-
 export default MovieList;
